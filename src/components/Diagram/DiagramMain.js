8b196dc54be524c40836a204a478ef2b7e372e0f
david hernquist <dhernquist@rentpath.com>
2018-10-15 12:53:31 -0400

    made DisplayComponent.js

--- src/components/Diagram/DiagramMain.js
+++ src/components/Diagram/DiagramMain.js
@@ -1,36 +1,9 @@
 import React, { Component, Fragment } from "react";
 import auth from "../HOC/auth";
 import ShowUnassigned from "./ShowUnassigned";
+import DisplayComponent from "./DisplayComponent";
 import helper from "../../helpers/helper";
-import {
-  DiagramMainContainer as Container,
-  ComponentContainer,
-  ComponentName,
-  Row
-} from "styles";
-
-const DisplayComponent = ({ component, parent, history }) => {
-  const { name, iteration } = component;
-  const stateOutput =
-    component.style === "presentational" ? "n/a" : component.state.length;
-  const border =
-    component.style === "container" ? "3px solid red" : "3px solid blue";
-  const backgroundColor =
-    parent === component._id ? "rgba(255, 0, 0, 0.5)" : "rgba(0, 0, 0, 0.1)";
-  const margin = "0 3px";
-  const styles = { border, backgroundColor, margin };
-
-  return (
-    <ComponentContainer
-      style={styles}
-      onClick={() => history.push(`/main/component/${name}/${iteration}`)}
-    >
-      <ComponentName>{component.name}</ComponentName>
-      <div>{`STATE: ${stateOutput}`}</div>
-      <div>{`PROPS: ${component.props.length}`}</div>
-    </ComponentContainer>
-  );
-};
+import { DiagramMainContainer as Container, Row } from "styles";
 
 const TreeRow = ({ row, parent, history }) => {
   return (
