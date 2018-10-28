ee1cf509b2166c6a0010162851cc45e1e7ba7c8f
David Hernquist <david_hernquist@gmail.com>
2018-10-28 00:05:07 -0400

    callback list is in check

--- src/styles/CallbackList.js
+++ src/styles/CallbackList.js
@@ -4,11 +4,21 @@ export const Callback = styled.div`
   font-size: 20px; 
   height: 28px;
   width: 90%;
+  max-width: 90%;
   border: 1px solid ${ props => props.theme.madison };
   background-color: ${ props => props.theme.lightGrey };
   color: ${ props => props.theme.teal };
   padding: 4px 10px;
-  display: flex;
+
+  &:last-child {
+    border-bottom-right-radius: 5px;
+    border-bottom-left-radius: 5px;
+  }
+
+  &:nth-child(2) {
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+  }
 
   &:hover {
     background-color: ${ props => props.theme.madison };
@@ -18,4 +28,8 @@ export const Callback = styled.div`
 
 export const CallbackListContainer = styled.div`
   padding-bottom: 10px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
