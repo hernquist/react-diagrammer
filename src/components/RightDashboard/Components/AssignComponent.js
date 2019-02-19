54243488ba7293a41bc0532798b3fc5ef4d76d2d
David Hernquist <david_hernquist1@gmail.com>
2019-02-19 08:58:05 -0500

    assign component

--- src/components/RightDashboard/Components/AssignComponent.js
+++ src/components/RightDashboard/Components/AssignComponent.js
@@ -4,6 +4,7 @@ import { ASSIGN_COMPONENT } from "../../../graphql/mutations";
 import ComponentList from "./StateAndProps/ComponentList";
 import { Buttons } from "styles";
 import { SubmitButton } from "components/UI/SubmitButton";
+import { RightDashboardButton as Button } from "../../UI/RightDashboardButton";
 import helper from "helpers/helper";
 
 export default class AssignComponent extends Component {
@@ -55,12 +56,14 @@ export default class AssignComponent extends Component {
               text="Choose a parent?"
             />
             <Buttons>
-              <SubmitButton
+              <Button
                 onClick={this.updateComponents(AssignComponent, highlighted)}
-              >
-                ASSIGN
-              </SubmitButton>
-              <SubmitButton onClick={this.close}>CANCEL</SubmitButton>
+                text="ASIGN"
+              />
+              <Button 
+                onClick={this.close}
+                text="CANCEL"
+              />
             </Buttons>
           </Fragment>
         )}
