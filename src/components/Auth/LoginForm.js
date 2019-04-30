36beb9674f6f0d5f19093589655c0b984353bbb5
David Hernquist <david_hernquist1@gmail.com>
2019-04-30 12:39:24 -0400

    playing with routes

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import { Mutation, Query } from "react-apollo";
 import { LOGIN } from "../../graphql/mutations";
 import { GET_AUTH_USER } from "../../graphql/queries";
-import { SubmitButton } from "../ui/SubmitButton";
-import Errors from "../UI/Errors";
+// import { SubmitButton } from "../UI/SubmitButton";
+// import Errors from "../UI/Errors";
 import {
   AuthWrapper,
   InputField,
@@ -74,12 +74,8 @@ class LoginForm extends Component {
                         onChange={e => this.handleInput(e, "password")}
                       />
                     </InputField>
-                    <Errors
-                      errors={errors}
-                      exception={"auth"}
-                      from={"LoginForm"}
-                    />
-                    <SubmitButton>Submit</SubmitButton>
+                    {/* <Errors errors={errors} exception={"auth"} from={"LoginForm"} /> */}
+                    {/* <SubmitButton>Submit</SubmitButton> */}
                   </Form>
                 )}
               </Mutation>
